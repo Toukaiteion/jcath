6c4187f808c95e7ac8eafee64d0dbc5507493033
daisikiu <hseea2015@163.com>
2026-05-08 22:24:02 +0800

    删除多余文件

--- jcatch/utils/__init__.py
+++ jcatch/utils/__init__.py
@@ -1,7 +1,6 @@
 """Utility functions."""
 
-from jcatch.utils.download import download_image
 from jcatch.utils.file import extract_number_from_path
 from jcatch.utils.downloader import ImageDownloader
 
-__all__ = ["download_image", "extract_number_from_path", "ImageDownloader"]
+__all__ = ["extract_number_from_path", "ImageDownloader"]
